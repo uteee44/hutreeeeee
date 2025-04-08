6a3587cbc341b7d4e273b6dc0cab5c10dec06505
uteee44 <aditya.zzxz0@gmail.com>
2025-04-08 10:23:56 +0530

    Update sp.sh

--- sp.sh
+++ sp.sh
@@ -7,3 +7,11 @@ sudo apt update && sudo apt install -y \
     squashfs-tools xsltproc zip zlib1g-dev \
     python3 python-is-python3 unzip p7zip-full openjdk-11-jdk \
     repo android-sdk-ext4-utils
+
+
+sudo apt install -y \
+    git-core gnupg flex bison build-essential \
+    zip curl zlib1g-dev gcc-multilib g++-multilib \
+    libc6-dev-i386 lib32ncurses-dev x11proto-core-dev \
+    libx11-dev lib32z-dev ccache libgl1-mesa-dev \
+    libxml2-utils xsltproc unzip fontconfig
